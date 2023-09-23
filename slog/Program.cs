2c9a773e583ae0aa11e7426d81c4c8067c954bd4
Andrei.Makash <andrei.makash@yandex.ru>
2023-09-23 22:42:29 +0300

    добавлен slog

---
+++ slog/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.InputEncoding = System.Text.Encoding.Unicode;
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+
+int max = a ;
+
+if (a > max ) max = a ;
+if (b > max ) max = b ;
+if (c > max ) max = c ;
+if (d > max ) max = d ;
+
+Console.Write("max =  ") ;
+Console.Write(max) ;
+
