5a86080bcd1a5969501078afa892ef158050334e
Andrei.Makash <andrei.makash@yandex.ru>
2023-09-23 19:25:59 +0300

    добавлен hello_Masha

---
+++ Hello_Masha/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Маша" )
+{
+    Console.WriteLine("Ура, это же Маша ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+
